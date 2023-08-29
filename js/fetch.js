9ac3257f02d08faf648acb0013cfaa824d0b6e3e
Daniel Benson <>
2023-08-29 13:58:05 +0100

    Update

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,7 @@
 //CRUD OPERATIONS USING FETCH API
 //Getting all Students into the table
 //---------------------------------------
+var userId;
 fetch('https://localhost:7007/api/Students/getstudentdb').then((data) => {
     // console.log(data); json format
     return data.json(); //Converted to Object
@@ -13,9 +14,9 @@ fetch('https://localhost:7007/api/Students/getstudentdb').then((data) => {
         <td>${values.middleName}</td>
         <td>${values.lastName}</td>
         <td>${values.department}</td>
-        <td>2023-HA-${values.id}</td>
+        <td>${values.regNumber}</td>
         <td>
-            <button id ="editStudent" data-id="${values.id}" onclick="editStudentModalFunction()">
+            <button id ="editStudent" data-id="${values.id}" onclick="getStudentInfo(${values.id})">
                 <span class="material-symbols-outlined">
                     edit
                 </span>
@@ -47,8 +48,8 @@ fetch('https://localhost:7007/api/Students/getstudentdb').then((data) => {
 fetch('https://localhost:7007/api/Students/getDeletedstudentdb').then((data) => {
     return data.json();
 }).then((objData) => {
-    let admittedStudents = objData.length;
-    return document.getElementById("deletedStudents").innerHTML = admittedStudents;
+    let deletedStudents = objData.length;
+    return document.getElementById("deletedStudents").innerHTML = deletedStudents;
 });
 
 
@@ -67,7 +68,6 @@ function getAddStudentInputValues(){
 
 //Adding a New Student with the Fetch API
 const form = document.getElementById('submitBtn');
-
 let buttonIsClicked = false;
 form.addEventListener('click', (e) =>{
 
@@ -75,6 +75,7 @@ form.addEventListener('click', (e) =>{
     buttonIsClicked = true;
     form.style.cursor = "not-allowed";
     form.disabled = true;
+
     const data = getAddStudentInputValues();
     console.log(data);
 
@@ -181,23 +182,107 @@ function deleteStudent(studentId) {
 
 }
 
-//Function to Edit Data
-function editStudent(studentId){
-    fetch(`https://localhost:7007/api/Students/getstudentbyid?id=${studentId}`)
+//Get Student Info for edit
+function getStudentInfo(studentId) {
+    userId = studentId
+    console.log(studentId);
+    fetch(`https://localhost:7007/api/Students/getstudentbyid?id=${studentId}`, {
+        method: "GET",
+    })
     .then((res) => res.json())
-    .then((data) =>{
-        console.log(data)
+    .then(data => {
+        console.log(data); // Optionally, log the response
+        
+        //this populates the edit inputs with previous data
+        document.getElementById("editFirstName").value= data.firstName;
+        document.getElementById("editMidName").value= data.middleName;
+        document.getElementById("editLastName").value= data.lastName;
 
-        document.getElementById('firstName').value = data.firstName;
-        document.getElementById('midName').value = data.middleName;
-        document.getElementById('lastName').value = data.lastName;
-        document.getElementById('department').value = data.department;
+        //This pops up the modal with the previous data
+        let editStudentModal = document.getElementById("myEditStudentModal");
+        editStudentModal.style.display = "block";
+
+        //this closes the modal
+        let closeEdit = document.getElementById('closeEditForm');
+        closeEdit.onclick = function(){
+            editStudentModal.style.display ="none";
+        }
+
+        //this closes the modal by touching the exterior regions of the modal window
+        window.onclick = function(event){
+            if(event.target == editStudentModal){
+                editStudentModal.style.display = "none";
+            }
+        }
     })
     .catch((error) => {
-        console.error("Error fething student for edit", error);
-    })
+        console.error("Error getting Student's info:", error);
+    });
 }
 
+function editStudentData() {
+    console.log(userId);
+    
+    
+}
+
+
+const editForm = document.getElementById('saveEditBtn');
+let EditButtonIsClicked = false;
+
+editForm.addEventListener('click', (e) => {
+    buttonIsClicked = true;
+    form.style.cursor = "not-allowed";
+    form.disabled = true;
+
+    var data = getnewData();
+    console.log(data);
+
+    function getnewData(){
+        return{
+            id: userId,
+            firstName: document.getElementById("editFirstName").value,
+            middleName: document.getElementById("editMidName").value,
+            lastName: document.getElementById("editLastName").value
+        }
+    }
+
+    fetch(`https://localhost:7007/api/Students/editstudent`, {
+        method: "PUT",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then((updatedStudent) => {
+        console.log("Student updated:", updatedStudent);
+    });
+    e.preventDefault();
+
+    $(function(){
+        toastr['info'](`Student edited Successfully, Kindly press the close button`, "Added Student", {timeOut: 5000});
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": false,
+            "positionClass": "toast-bottom-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+    });
+
+    setTimeout(reFresh, 5000)
+})
 
 //Function to refresh page
 function reFresh(){
--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-//Adding a New Student
 let addStudentModal = document.getElementById("myaddStudentModal");
 let addStudentBtn = document.getElementById("addStudent");
 let span = document.getElementsByClassName("close")[0];
@@ -17,35 +15,4 @@ window.onclick = function(event){
         addStudentModal.style.display = "none";
     }
 }
-
-//Editing an existing User
-//----------------------------
-//Declaring the Edit form Modal from the HTML
-let editStudentModal = document.getElementById("myEditStudentModal");
-
-//Function to be triggered by an edit button click
-function editStudentModalFunction(){
-    editStudentModal.style.display = "block";
-    //let editStudentBtn = document.getElementById("editStudent");
-
-}
-
-function closeModal(){
-    let editSpan = document.getElementsByClassName("close");
-
-    editSpan.onclick = editStudentModal.style.display = "none";
-    window.onclick = function(event){
-        if(event.target == editStudentModal){
-            editStudentModal.style.display = "none";
-        }
-    }
-
-}
-
-
-
-    
-    
-
-
 
